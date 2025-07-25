fc3e2b5ad7d3f621c0580da5a1456c766d2e0fff
I545843 <julian.konz@sap.com>
2025-07-25 14:45:28 +0200

    fixed all the issues again booyaaa

--- eventim-disability-integration/src/pages/admin/tours/index.jsx
+++ eventim-disability-integration/src/pages/admin/tours/index.jsx
@@ -448,7 +448,7 @@ export default function ToursContent() {
                                                 ? URL.createObjectURL(editedData.tour_image)
                                                 : tour.tour_image
                                                     ? `${API_BASE_URL}/image/${tour.tour_image}`
-                                                    : '/placeholder-tour.png'
+                                                    : '/pictures/placeholder.png'
                                         }
                                         alt={tour.title || 'Unbekannte Tour'}
                                     />
